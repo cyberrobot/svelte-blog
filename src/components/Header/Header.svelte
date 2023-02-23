797002cf3824bf234b593e3727ee5d07c0690256
Yaroslav Salmin <yaroslav.shoppertrak@gmail.com>
2023-02-23 16:28:43 +0000

    Logo component in header

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Logo from '../Logo/Logo.svelte';
+</script>
+
+<div class="p-8">
+	<Logo />
+	Header
+</div>
---
+++ src/components/Logo/Logo.svelte
@@ -0,0 +1,10 @@
+<div class="logo" />
+
+<style>
+	.logo {
+		background: url('./images/logo-v3.svg') no-repeat center center;
+		background-size: contain;
+		width: 110px;
+		height: 54px;
+	}
+</style>
